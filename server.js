2d97f988f350da8c5cf72f43572fd9c3c6063036
anishasnet <anishasnet@gmail.com>
2020-05-24 14:34:10 -0700

    added GET requests

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path'); 
+const app = express(); 
 
 const PORT = process.env.PORT || 3001;
 
-const app = express(); 
+
+
+app.use(express.json());
+app.use(express.urlencoded ( { extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
+
+app.get('/api/notes', (req, res) => {
+    return res.sendFile(path.join(__dirname, '/db/', 'db.json'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
+
