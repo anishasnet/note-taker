24072af12b2e261beb9d74b8822134b8f5e39d8e
anishasnet <anishasnet@gmail.com>
2020-05-24 22:37:50 -0700

    added comments to code, and images for README

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+/* Dependencies */
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path'); 
 const app = express(); 
 
+/* Create local host port */
 const PORT = process.env.PORT || 3001;
 
+/* Middleware */
 app.use(express.urlencoded ( { extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
+/* HTML routes */
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
@@ -17,9 +22,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 }); 
 
+/* API routes */
 app.get('/api/notes', (req, res) => {
     return res.sendFile(path.join(__dirname, '/db/', 'db.json'));
 });
+
+/* Checks if note has valid content */
 
 function checkNote (noteToCheck) {
     if (!noteToCheck.title || typeof noteToCheck.title !== 'string') {
@@ -33,6 +41,7 @@ function checkNote (noteToCheck) {
     }
 };
 
+/* API route to post the note */
 app.post('/api/notes', (req, res) => {
     let note = req.body;
     let allNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -47,6 +56,8 @@ app.post('/api/notes', (req, res) => {
         res.json(allNotes);
     }
 });
+
+/* API route to delete a given note */
 app.delete('/api/notes/:id', (req, res) => {
     let allNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let toDeleteId = req.params.id;
@@ -54,17 +65,15 @@ app.delete('/api/notes/:id', (req, res) => {
     allNotes = allNotes.filter(note => {
         return note.id != toDeleteId;
     });
-
     for (note of allNotes) {
         note.id = newId.toString();
-        newId++
+        newId++;
     }
-    
     fs.writeFileSync("./db/db.json", JSON.stringify(allNotes));
     res.json(allNotes);
 });
 
-
+/* Port is launched for server */
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
 });
